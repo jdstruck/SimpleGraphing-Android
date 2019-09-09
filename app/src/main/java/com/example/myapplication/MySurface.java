69ff02c1244de9e16ad000b154b192c8074c41ca
Jesse Struck <jdstruck@gmail.com>
2019-09-09 10:23:33 -0400

    Add initial extended SurfaceView class

---
+++ app/src/main/java/com/example/myapplication/MySurface.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder = null;
+    private Paint paint = null;
+    private float circleX = 0;
+    private float circleY = 0;
+
+    public MySurface(Context context) {
+        super(context);
+
+        surfaceHolder = getHolder();
+
+        paint = new Paint();
+        paint.setColor(Color.RED);
+
+        // this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        // paint.setStyle(Style.FILL);
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
